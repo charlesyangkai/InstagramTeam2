fe062786af63004006f7a28603f88537aac86117
charleslee <charleslee@mbp.local>
2017-02-10 17:00:41 +0800

    Initial Commit

---
+++ InstagramTeam2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  InstagramTeam2
+//
+//  Created by Charles Lee on 10/2/17.
+//  Copyright © 2017 NextAcademy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
